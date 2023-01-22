f960af1fac3e6f0b5886dcc7a5c91e3a8cb3423e
fake1 <manaliphotos111@gmail.com>
2023-01-22 19:27:30 +0530

    User verification is now necessary

--- app/src/main/java/com/example/hotelmanagernith/LoginActivity.kt
+++ app/src/main/java/com/example/hotelmanagernith/LoginActivity.kt
@@ -3,26 +3,59 @@ package com.example.hotelmanagernith
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.example.hotelmanagernith.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding=ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-        val intentReg= Intent(this,SignUp::class.java)
-        val intentHom= Intent(this,HomeActivity::class.java)
-
-        val buttonSig=findViewById<Button>(R.id.button)
-        val buttonReg=findViewById<Button>(R.id.button2)
-
-        buttonReg.setOnClickListener(){
-            startActivity(intentReg)
+        firebaseAuth=FirebaseAuth.getInstance()
+        binding.button2.setOnClickListener{
+            val intent =Intent(this,SignUp::class.java)
+            startActivity(intent)
         }
-        buttonSig.setOnClickListener(){
-            startActivity(intentHom)
+
+        binding.button.setOnClickListener{
+            val email=binding.editTextTextPersonName.text.toString()
+            val pass = binding.editTextTextPersonName3.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+
+                    firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val verification=firebaseAuth.currentUser?.isEmailVerified
+                            if(verification==true) {
+                                val intent = Intent(this, HomeActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                            else{
+                                Toast.makeText(this, "Please verify your email", Toast.LENGTH_SHORT).show()
+                            }
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                            val ans=it.exception.toString()
+                        }
+
+                    }
+
+            }else{
+                    Toast.makeText(this, "Incorrect username and password", Toast.LENGTH_SHORT).show()
+            }
+
         }
+
 
     }
 }
--- app/src/main/java/com/example/hotelmanagernith/SignUp.kt
+++ app/src/main/java/com/example/hotelmanagernith/SignUp.kt
@@ -4,24 +4,60 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.hotelmanagernith.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        binding =ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        firebaseAuth=FirebaseAuth.getInstance()
 
+        binding.button.setOnClickListener{
+            val email=binding.editTextTextPersonName2.text.toString()
+            val pass = binding.editTextTextPersonName5.text.toString()
+            val confirmPass=binding.editTextTextPersonName.text.toString()
 
-        val intentHom=Intent(this,HomeActivity::class.java)
+            if(email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if(pass==confirmPass){
 
-        val buttonSig=findViewById<Button>(R.id.button)
+                    firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener{
+                        if(it.isSuccessful){
 
-        buttonSig.setOnClickListener(){
-            startActivity(intentHom)
+                            firebaseAuth.currentUser?.sendEmailVerification()
+                                ?.addOnSuccessListener {
+                                    Toast.makeText(this,
+                                        "Click on the link sent in email to verify your account",
+                                        Toast.LENGTH_LONG).show()
+                                }
+                                ?.addOnFailureListener{
+                                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+                                }
+                            val intent=Intent(this,LoginActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                }else{
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Empty fields are not allowed !!", Toast.LENGTH_SHORT).show()
+            }
         }
-
-
 
     }
 }

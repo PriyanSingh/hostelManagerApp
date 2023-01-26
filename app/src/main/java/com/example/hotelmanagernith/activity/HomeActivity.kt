3b0432cb3684a8c767df54d6d0f7e1704a21f9ad
deveshpandee <devesh2051@gmail.com>
2023-01-26 15:13:58 +0530

    UI Improved

--- app/src/main/java/com/example/hotelmanagernith/activity/HomeActivity.kt
+++ app/src/main/java/com/example/hotelmanagernith/activity/HomeActivity.kt
@@ -2,15 +2,21 @@ package com.example.hotelmanagernith.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.hotelmanagernith.*
+import com.example.hotelmanagernith.databinding.ActivityHomeBinding
 import kotlinx.android.synthetic.main.activity_home.*
+import java.util.zip.Inflater
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val profileFragement= ProfileFragment()
         val outpassFragment= OutpassFragment()
@@ -18,14 +24,14 @@ class HomeActivity : AppCompatActivity() {
 
         makeCurrentFragment(profileFragement)
 
-        bottomNavigationView.setOnNavigationItemSelectedListener {
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
             when(it.itemId){
                 R.id.profile ->makeCurrentFragment(profileFragement)
                 R.id.outpass ->makeCurrentFragment(outpassFragment)
             }
             true
         }
-        fab.setOnClickListener{
+        binding.fab.setOnClickListener{
             makeCurrentFragment(allotmentFragment)
         }
 

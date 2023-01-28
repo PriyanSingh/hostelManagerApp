0cfcc8e1bb93848f8bc5dc0dd97a4c2d7dc7c08b
priyanshusingh <priyanshusingh0520@gmail.com>
2023-01-28 21:23:43 +0530

    fragment layout as delete and new room layout as been add

---
+++ app/src/main/java/com/example/hotelmanagernith/activity/RoomDetails.kt
@@ -0,0 +1,11 @@
+package com.example.hotelmanagernith.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_details)
+    }
+}
